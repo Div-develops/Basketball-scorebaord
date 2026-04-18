4f2bf49e03c02e47cdd967f8ce2884436ca3c71f
Divya Pawar <divyapawar.sstc@gmail.com>
2026-04-18 11:37:21 +0530

    wrote my own code for the project
    wrote my own code for the project

---
+++ index.js
@@ -0,0 +1,22 @@
+let sb1 = document.getElementById("score-board1")
+let sb2 = document.getElementById("score-board2")
+function increment1_1(){
+ sb1.textContent = Number(sb1.textContent) + 1;
+}
+function increment2_1(){
+    sb1.textContent = Number(sb1.textContent) + 2;
+}
+function increment3_1(){
+    sb1.textContent = Number(sb1.textContent) + 3;
+}
+
+
+function increment1_2(){
+ sb2.textContent = Number(sb2.textContent) + 1;
+}
+function increment2_2(){
+    sb2.textContent = Number(sb2.textContent) + 2;
+}
+function increment3_2(){
+    sb2.textContent = Number(sb2.textContent) + 3;
+}
